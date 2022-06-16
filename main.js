a7d918ee76d7eae5d485dc06831f72256a213a68
Numaira222 <77472414+Numaira222@users.noreply.github.com>
2022-06-15 17:38:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 function setup(){
 video = createCapture(VIDEO);
-video.size(500,500);
-canvas = createCanvas(500,500);
+video.size(400,400);
+canvas = createCanvas(400,400);
 canvas.position(560,150);
 poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on("pose", gotPoses);
@@ -19,7 +19,7 @@ function modelLoaded(){
     console.log("Model is initialized");
 }
 
-function gotPoses(){
+function gotPoses(results){
     if(results.length>0){
        console.log(results);
     }
